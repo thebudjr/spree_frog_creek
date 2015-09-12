6b13f102028accd07709a25f8f8f186c2e889f05
thebudjr <tim.s.janney@gmail.com>
2015-09-12 15:01:22 -0400

    tinkered with deface, removed search bar

---
+++ app/overrides/replace_logo.rb
@@ -0,0 +1,4 @@
+#Deface::Override.new(:virtual_path => "spree/shared/_header", 
+#                     :name => "replace_logo", 
+#                     :replace => ".row", 
+#                     :text => "<h1>Frog Creek Photography</h1>")
